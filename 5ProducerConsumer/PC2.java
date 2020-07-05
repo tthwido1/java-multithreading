2dbec7f83a48a2db58bad27af3dc0dd62c71cca8
Spring-Green-arch <vishaldhage27@gmail.com>
2020-07-05 17:45:55 +0530

    fix(PC2 bug ; use diff locks for await and signal)

--- 5ProducerConsumer/PC2.java
+++ 5ProducerConsumer/PC2.java
@@ -43,21 +43,33 @@ class PCLockHelper {
     private final Lock rlock = new ReentrantLock();
     Condition condition = rlock.newCondition();
 
+    private final Lock rlock1 = new ReentrantLock();
+    Condition condition1 = rlock1.newCondition();
+
+    private final Lock rlock2 = new ReentrantLock();
+    Condition condition2 = rlock2.newCondition();
+
     public void produces() throws InterruptedException {
         while (true) {
             if (list.size() < LIMIT) {
                 rlock.lock();
-                this.val++;
-                System.out.println("T1 Adding val: " + val);
-                list.add(this.val);
+                while (list.size() != LIMIT) {
+                    this.val++;
+                    System.out.println("T1 Adding val: " + val);
+                    list.add(this.val);
+                }
                 rlock.unlock();
             } else {
                 System.out.println("T1 List full, waiting ...");
                 this.val = 0;
-                rlock.lock();
-                condition.signal();
-                condition.await();
-                rlock.unlock();
+
+                rlock1.lock();
+                condition1.signal();
+                rlock1.unlock();
+
+                rlock2.lock();
+                condition2.await();
+                rlock2.unlock();
             }
             Thread.sleep(500);
         }
@@ -68,13 +80,18 @@ class PCLockHelper {
         while (true) {
             if (list.isEmpty()) {
                 System.out.println("T2 Nothing to consume, so wait ...");
-                rlock.lock();
-                condition.signal();
-                condition.await();
-                rlock.unlock();
+
+                rlock2.lock();
+                condition2.signal();
+                rlock2.unlock();
+
+                rlock1.lock();
+                condition1.await();
+                rlock1.unlock();
             } else {
                 rlock.lock();
-                System.out.println("T2 Consume val: " + list.remove(0));
+                while (!list.isEmpty())
+                    System.out.println("T2 Consume val: " + list.remove(0));
                 rlock.unlock();
             }
             Thread.sleep(500);
